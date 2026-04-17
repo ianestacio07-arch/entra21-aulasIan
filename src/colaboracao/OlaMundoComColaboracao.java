9a4fda3231841f37e59b05a2467cf9f1467250d0
Fernando Maciel <fernando.maciel@prof.sc.senac.br>
2026-04-17 19:07:43 -0300

    Adicionado OlaMundo para CR

---
+++ src/colaboracao/OlaMundoComColaboracao.java
@@ -0,0 +1,13 @@
+package colaboracao;
+
+public class OlaMundoComColaboracao {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo com colaboração!");
+        System.out.println("Estou no repositorio do IAN!");
+        System.out.println("Entrei no Ian!");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Qualquer coisa");
+        }
+    }
+}
